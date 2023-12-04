ac186c8a8d7c0b17112dc6366e51a384086f009b
Deveshvilkar <dvshvilkar67@gmail.com>
2023-12-04 18:57:51 +0530

    done

---
+++ E-Commerce/src/main/java/com/jsp/ECommerce/model/ResponseStructure.java
@@ -0,0 +1,37 @@
+package com.jsp.ECommerce.model;
+
+public class ResponseStructure<T> {
+	private T data;
+	private String message;
+	private int status_code;
+	private AuthenticationToken token;
+	
+	
+	public AuthenticationToken getToken() {
+		return token;
+	}
+	public void setToken(AuthenticationToken token) {
+		this.token = token;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getStatus_code() {
+		return status_code;
+	}
+	public void setStatus_code(int status_code) {
+		this.status_code = status_code;
+	}
+	
+	
+
+}
---
+++ E-Commerce/src/main/java/com/jsp/ECommerce/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.jsp.ECommerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jsp.ECommerce.dao.ProductDao;
+import com.jsp.ECommerce.exception.ProductNotFoundException;
+import com.jsp.ECommerce.model.Product;
+
+
+@Service
+public class ProductService {
+	@Autowired
+	ProductDao productDao;
+	
+	public void saveProduct(Product product) {
+		productDao.saveProduct(product);
+	}
+	
+	public List<Product> getAllProducts(){
+		return productDao.getAllProducts();
+	}
+	
+	public Product getProductById(int id) throws ProductNotFoundException {
+		return productDao.getProductById(id);
+	}
+	
+	public boolean deleteProduct(int id) {
+		return productDao.deleteProduct(id);
+	}
+	
+	public Product updateCategory(int id,Product product) throws ProductNotFoundException {
+		Product p=productDao.getProductById(id);
+		if (p!=null) {
+			p.setName(product.getName());
+			p.setImageurl(product.getImageurl());
+			p.setCategory(product.getCategory());
+			p.setDescription(product.getDescription());
+			p.setPrice(product.getPrice());
+			return productDao.updateProduct(id,p);
+		}else {
+			return null;
+		}
+	}
+
+}
